f5b5b069d4e4886b0715aa3af3254e6bb766870a
EPifAnovVladIMIR <95338800+EPifAnovVladIMIR@users.noreply.github.com>
2023-04-19 12:25:29 +0300

    Добавил комментарии на проект

---
+++ program_Epifanov/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_Epifanov
+{
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите кол-во самолётов");
+            int count = Convert.ToInt32(Console.ReadLine());
+ 
+            PlaneControl plane = new PlaneControl(count);
+            plane.Fill(); //метод на заполнение массива
+            plane.Sort(); // метод на сортировку массива 
+ 
+            Console.Read();     
+
+        }
+    }
+}
+                
+
+      
